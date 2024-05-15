bce9ef0a78cd4fc53e719ca20b0f3551884bad69
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-15 17:42:04 +0500

    lazy to give names

--- communication.py
+++ communication.py
@@ -1,37 +1,16 @@
-# from typing import Any
-# from rust_compiler import compile_rust_folder
+from typing import Any
+from rust_compiler import compile_rust_folder
 
-# def load_function(dll, name, arguments=None, return_type=None):
-#     func = getattr(dll, name)
-#     if arguments: func.argtypes = arguments
-#     if return_type: func.restype = return_type
+def load_function(dll, name, arguments=None, return_type=None):
+    func = getattr(dll, name)
+    if arguments: func.argtypes = arguments
+    if return_type: func.restype = return_type
 
-#     return func
+    return func
 
-# if __name__ == '__main__':
-#     # Testing library
-#     dll = compile_rust_folder()
-#     counter = dll.Counter_new(50)
-#     dll.Counter_count(counter)
-#     print(dll.Counter_get(counter))
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Define the range of values for o
-o_values = np.linspace(0, 10, 400)  # Adjust the range as needed
-
-# Define the constant parameter a
-a = 5  # Adjust the value of 'a' as needed
-
-# Calculate r for each value of o
-r_values = np.sin((a / 5) * o_values)
-
-# Plot the curve
-plt.plot(o_values, r_values, label=r'$r = \sin\left(\frac{a}{5} \cdot o\right)$')
-plt.xlabel('o')
-plt.ylabel('r')
-plt.title('Visualization of r = sin(a/5 o)')
-plt.legend()
-plt.grid(True)
-plt.show()
+if __name__ == '__main__':
+    # Testing library
+    dll = compile_rust_folder()
+    counter = dll.Counter_new(50)
+    dll.Counter_count(counter)
+    print(dll.Counter_get(counter))
