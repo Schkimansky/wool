6ca45894435ebe4d81332a0cbfd19f345282e7bf
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-17 23:59:26 +0500

    support for py strings

--- communication.py
+++ communication.py
@@ -1,9 +1,19 @@
 from typing import Any
 import ctypes as c
 
-C_TYPES = {'int': c.c_int, 'str': c.c_char_p, int: c.c_int, str: c.c_char_p}
+C_TYPES = {'bool': c.c_bool,   bool: c.c_bool,
+           'int':  c.c_int,    int: c.c_int, 
+           'str':  c.c_char_p, str: c.c_char_p}
 
 class DataType: pass
+
+def check_array_consists_of_one_type(arr):
+    start_type = type(arr[0])
+    for item in arr:
+        if type(item) != start_type:
+            raise ValueError(f"A python list sent to rust must be of the same type, in this array: {arr} the value ({item}) has a different type than the other rest")
+        else:
+            return start_type # Every item consists of the same type, So just return the type of the first element.
 
 def load_function(function, arguments=None, return_type=None):
     func = function
@@ -17,6 +27,9 @@ def load_function(function, arguments=None, return_type=None):
         for paramval in parameter_values:
             if type(paramval) == str:
                 processed_values.append(paramval.encode())
+            if type(paramval) == list:
+                array_type = check_array_consists_of_one_type(paramval)
+                processed_values.append((C_TYPES[array_type] * len(paramval))(*paramval))
             else:
                 processed_values.append(paramval)
 
@@ -24,7 +37,7 @@ def load_function(function, arguments=None, return_type=None):
 
     return funtion_caller
 
-def make_rust_class(class_name: str, parameters: dict[str, DataType]):
+def load_rust_class(class_name: str, parameters: dict[str, DataType]):
 
     class RustClass(c.Structure):
         _fields_ = [(key, C_TYPES[val]) for key, val in parameters.items()]
